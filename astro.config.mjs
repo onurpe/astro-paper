a5d6146bc5a53715dbfb053cdd2820c48314a719
onur pe <onur@kriko.com.tr>
2023-04-04 13:27:19 +0300

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+  site: "https://urbangardener.wiki/",
   integrations: [
     tailwind({
       config: {
